5b879f9f951c9cb8d0f60bb3ba52d75a613fec16
karman103 <karman103@gmail.com>
2025-07-30 21:58:07 -0400

    Sync repo: reflect current file structure

--- freeciv-3.1.5/server/scripting/tolua_fcdb_gen.h
+++
@@ -1,9 +0,0 @@
-/*
-** Lua binding: fcdb
-** Generated automatically by tolua 5.2.4 on Fri May  9 01:06:50 2025.
-*/
-
-/* Exported function */
-TOLUA_API int tolua_fcdb_open (lua_State* tolua_S);
-LUALIB_API int luaopen_fcdb (lua_State* tolua_S);
-
--- freeciv-3.1.5/server/scripting/tolua_server_gen.h
+++
@@ -1,9 +0,0 @@
-/*
-** Lua binding: server
-** Generated automatically by tolua 5.2.4 on Fri May  9 01:06:50 2025.
-*/
-
-/* Exported function */
-TOLUA_API int tolua_server_open (lua_State* tolua_S);
-LUALIB_API int luaopen_server (lua_State* tolua_S);
-
