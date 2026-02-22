f04683fc3a8cdf6e22e4c65931849d62769dc71b
karman103 <karmanbir103@gmail.com>
2026-02-21 19:00:27 -0800

    its fucking workig

--- games/freeciv-docker/start.sh
+++ games/freeciv-docker/start.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
-STREAM_DIR=/tmp/streams
-mkdir -p "$STREAM_DIR"
 export DISPLAY=:99
 export PATH="/usr/games:$PATH"
+RTP_HOST="${RTP_HOST:-host.docker.internal}"
+RTP_PORT="${RTP_PORT:-5004}"
 mkdir -p /tmp/saves
 
 # start virtual display (lower res to reduce encoding cost)
@@ -21,14 +21,14 @@ sleep 2
 # start client connecting to local server
 freeciv-gtk3.22 --server localhost --port 5556 --name player >/tmp/freeciv-client.log 2>&1 &
 
-# start ffmpeg capture to HLS
+# start ffmpeg capture and publish RTP for WebRTC ingest on the host
 ffmpeg -hide_banner -loglevel error \
-  -f x11grab -video_size 960x540 -framerate 15 -i :99.0 \
+  -f x11grab -video_size 960x540 -framerate 20 -i :99.0 \
   -use_wallclock_as_timestamps 1 -fflags nobuffer -flags low_delay \
   -probesize 32 -analyzeduration 0 -flush_packets 1 \
   -an -c:v libx264 -preset ultrafast -tune zerolatency -pix_fmt yuv420p \
   -threads 2 -x264-params "scenecut=0:rc-lookahead=0:sync-lookahead=0" \
-  -g 2 -keyint_min 2 -sc_threshold 0 \
-  -b:v 900k -maxrate 1000k -bufsize 300k \
-  -f hls -hls_time 0.1 -hls_list_size 60 -hls_flags append_list+independent_segments+program_date_time \
-  -hls_segment_filename "$STREAM_DIR/segment_%03d.ts" "$STREAM_DIR/playlist.m3u8"
+  -profile:v baseline -level 3.1 \
+  -g 20 -keyint_min 20 -sc_threshold 0 \
+  -b:v 1200k -maxrate 1400k -bufsize 600k \
+  -payload_type 96 -f rtp "rtp://${RTP_HOST}:${RTP_PORT}?pkt_size=1200"
